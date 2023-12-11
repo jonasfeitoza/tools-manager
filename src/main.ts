fbc631316f0c1553facd73a5c6e14c7c1a230432
Jonas Feitoza <jonas.soaress@gmail.com>
2023-12-10 23:46:21 -0300

    Prettier sanitize

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import {
